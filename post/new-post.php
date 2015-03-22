4de3f84414be1a387b1f2d2c542820f5fc0dcfd6
Richard <brushric@msu.edu>
2015-03-21 21:57:38 -0400

    Post page for new user and updated login post.

--- post/new-post.php
+++ post/new-post.php
@@ -9,22 +9,17 @@
 $login = true;
 require '../lib/site.inc.php';
 
-if(isset($_POST['create']) or isset($_POST['New User'])){
-    header("location: ../newuser.php");
-    exit;
-}
-
 if(isset($_POST['userName']) && isset($_POST['password'])) {
     $users = new Users($site);
 
     $user = $users->login($_POST['userName'], $_POST['password']);
     if($user !== null) {
-        $_SESSION['user'] = $user;
+        $_SESSION['userName'] = $user;
         header("location: ../");
         exit;
     }
 }
 
-header("location: ../login.php?invalid");
+header("location: ../index.php");
 
 ?>
