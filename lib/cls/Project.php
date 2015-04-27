1705d86b6d2704998b77999f12eca498bb50bfe6
David Wegscheid <dlwegscheid@gmail.com>
2015-04-26 21:39:29 -0400

    Added projects pages

--- lib/cls/Project.php
+++ lib/cls/Project.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: David
- * Date: 4/26/2015
- * Time: 8:40 PM
- */
 
-// INSERT INTO
+// make new user SQL statements
+// INSERT INTO Project(title, creator) VALUES (?, ?); $title, $currentUser
+// INSERT INTO Collaborators(idUser, idProject, confirmed) SELECT ?, idProject, 1 FROM Project WHERE title=? AND creator=?; $currentUser, $title, $currentUser
+
+// find all projects w/ current user
+// SELECT P.Title FROM Project P, Collaborators C WHERE P.idProject=C.idProject AND C.idUser=?; $currentUser
+
+// find all collaborators for given project
+// SELECT idUser FROM Collaborators WHERE idProject=?; $projectID
